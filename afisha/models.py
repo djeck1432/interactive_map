e25fa5fd7607c88d275a3788aa67c2d2799bcac6
djeck1432 <oleh.kostashchuk@gmail.com>
2020-06-04 19:15:49 +0200

    Add Places model

--- afisha/models.py
+++ afisha/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-print('Hello!')
+class Places(models.Model):
+    title = models.CharField('Названия места',max_length=150)
+    images = models.ImageField('Изображения')
+    description_short = models.CharField('Краткое описания', max_length=300)
+    description_long = models.TextField('Полное описания')
+    long = models.FloatField('Долгота')
+    lat = models.FloatField('Широта')
+
+    def __str__(self):
+        return self.title
+
