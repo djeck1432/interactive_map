fe766a83bbfd224f1e8e6bf8d3dc0d640ca32a5b
djeck1432 <oleh.kostashchuk@gmail.com>
2020-06-04 19:26:21 +0200

    Add model for images

--- afisha/admin.py
+++ afisha/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Places
+from .models import Places, PlaceImages
 
 
 
 # Register your models here.
 admin.site.register(Places)
+admin.site.register(PlaceImages)
--- afisha/models.py
+++ afisha/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Places(models.Model):
     title = models.CharField('Названия места',max_length=150)
-    images = models.ImageField('Изображения')
+
     description_short = models.CharField('Краткое описания', max_length=300)
     description_long = models.TextField('Полное описания')
     long = models.FloatField('Долгота')
@@ -10,4 +10,14 @@ class Places(models.Model):
 
     def __str__(self):
         return self.title
+
+class PlaceImages(models.Model):
+    place = models.ForeignKey(
+                                Places, on_delete=models.CASCADE,
+                                verbose_name='Место',
+                                related_name='image')
+    images = models.ImageField('Изображения')
+
+
+
 
