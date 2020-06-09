3abd4d815ae43b267cd579e3a6346eb3db23d6ac
djeck1432 <djeck1432@users.noreply.github.com>
2020-06-09 20:25:01 +0200

    Update settings.py

--- interactive_website/settings.py
+++ interactive_website/settings.py
@@ -123,8 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
+    os.path.join(BASE_DIR, "static")
 ]
 STATIC_ROOT = '/static/'
 MEDIA_URL = '/media/'
